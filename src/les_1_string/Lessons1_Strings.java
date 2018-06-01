cf7803c10116b899b66cf3572e9efd6d4f49602e
rttr <rydui403@gmail.com>
2018-06-01 14:13:18 +0300

    add class lesson1 Строковые операторы

---
+++ src/les_1_string/Lessons1_Strings.java
@@ -0,0 +1,8 @@
+package les_1_string;
+
+public class Lessons1_Strings {
+    public static void main(String[] args) {
+        String [] food = {"food1 ", "food2", "food3", "food4", "5food", "food6"};
+
+    }
+}

f0867dfed34c62708d93a571195a094661af80aa
LinuxRTTR <rydui403@mail.com>
2021-02-23 09:26:20 +0200

    dont remember what...

--- eclipsePractice/Test/src/Main.java
+++ eclipsePractice/Test/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Arrays;
+import java.util.Iterator;
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello this is  test");// TODO Auto-generated method stub
+		
+		int arg[] = {1,2,3,4,5,6,7,8,9};
+		int leght = arg.length;
+		for (int i = 0; i < leght/2; i++) {
+			int temp = arg[i];
+			arg[i] = arg[leght-1-i];
+			arg[leght-1-i] = temp;
+		}
+		System.out.print(Arrays.toString(arg));
+		// TODO Auto-generated method stub
 
 	}
 
